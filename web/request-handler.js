ce2b0198a4f61fdaec55879ae9226768fbaf1a16
zfisch <zacharyrobert@gmail.com>
2015-04-16 12:27:15 -0700

    completed download and archive functionality

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,6 +1,7 @@
 var path = require('path');
+var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
-var http = require('http-request');
+// var http = require('http-request');
 var helpers = require('./http-helpers.js');
 // require more modules/folders here!
 
@@ -10,11 +11,37 @@ exports.handleRequest = function (req, res) {
 
   console.log(req.url);
   console.log(req.method);
+  archive.downloadUrls();
+
 
   //handle GET requests
   if(req.method === 'GET' && req.url === '/'){
     statuscode = 200;
-    responseData = '<input';
+    fs.readFile(archive.paths.siteAssets + '/index.html', function (err,data) {
+      if (err) {
+        console.log('error',archive.paths.siteAssets + 'index.html');
+        statuscode = 404;
+        responseData = JSON.stringify(err);
+        return;
+      } else{
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+      }
+    });
+  } else if(req.method === 'GET' && req.url === '/styles.css'){
+    console.log('asked for style sheet')
+    fs.readFile(archive.paths.siteAssets + '/styles.css', function (err,data) {
+      if (err) {
+        console.log('error',archive.paths.siteAssets + 'index.html');
+        statuscode = 404;
+        responseData = JSON.stringify(err);
+        return;
+      } else{
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        res.end(data);
+      }
+    });
+
   } else if(req.method === 'GET' && req.url[0] === '/' && req.url.length > 1){
     var filename = req.url.substring(1);
     if (archive.isURLArchived(filename)){
@@ -22,18 +49,35 @@ exports.handleRequest = function (req, res) {
       responseData = archive.getDataFromFile(filename);
     }
   } else if(req.method === 'POST' && req.url[0] === '/'){
+    console.log('in post');
     var data = '';
     req.on('data',function(chunk){
       data +=chunk;
     });
     req.on('end',function(){
+      console.log('data: ', data);
       var filename = data.substring(4);
       if(!archive.isUrlInList(filename) && !archive.isURLArchived(filename)){
+        console.log('in the if block');
         statuscode = 302;
         archive.addUrlToList(filename);
       }
+      fs.readFile(archive.paths.siteAssets + '/loading.html', function (err,data) {
+        if (err) {
+          console.log('error',archive.paths.siteAssets + 'loading.html');
+          statuscode = 404;
+          responseData = JSON.stringify(err);
+          return;
+        } else{
+          console.log('getting loading.html',data);
+          res.writeHead(302, {'Content-Type': 'text/html'});
+          res.end(data);
+        }
+      });
     });
   }
-  res.writeHead(statuscode, helpers.headers);
-  res.end(responseData);
+  console.log(helpers.headers);
+  //res.writeHead(statuscode, helpers.headers);
+  //res.end(responseData);
+  // res.end();
 };
